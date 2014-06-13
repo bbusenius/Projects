8cf67d0ff34e673d51d4611d235670a63ebbbf21
Brad Busenius <bbusenius@users.noreply.github.com>
2014-06-12 21:10:31 -0500

    Added new methods to the TwentyFortyEight class in Principles_of_Computing

--- Principles_of_Computing/2048.py
+++ Principles_of_Computing/2048.py
@@ -3,6 +3,7 @@ Clone of 2048 game.
 """
 
 #import poc_2048_gui        
+import random
 
 # Directions, DO NOT MODIFY
 UP = 1
@@ -19,25 +20,24 @@ OFFSETS = {UP: (1, 0),
    
 def merge(line):
     """
-    Helper function that merges a single row or column in 2048
+    Helper function that merges a single row or column in 2048.
     """
+    # Blank results list matching the length of the line
     results = [0] * len(line)
     index = 0
+    # Loop over the line and put numbers that aren't 0 together in the results
     for tile in line:
         if tile != 0:
             results[index] = tile
             # Only advance the counter if it's not a 0
             index += 1
-
-    index = 0
-    merged = False
-    for num in range(1, len(results)):
-        if results[index] == results[index - 1] and merged == False:
-            results[index - 1] = results[index] * 2
-            results.pop(index)
+    # Loop over the results and merge numbers
+    for number in range(len(results)):
+        if number > 0 and results[number] == results[number - 1]:
+            results[number - 1] = results[number] * 2
             results.append(0)
-            merged = True
-        index += 1
+            results.pop(number)
+   
     return results
 
 class TwentyFortyEight:
@@ -46,39 +46,39 @@ class TwentyFortyEight:
     """
 
     def __init__(self, grid_height, grid_width):
-        # replace with your code
-        pass
+        """Initialize the object."""
+        self._grid_height = grid_height
+        self._grid_width = grid_width
+        self._grid = ''
 
-    def test(self):
-        return "foobar"
-    
     def reset(self):
         """
         Reset the game so the grid is empty.
         """
-        # replace with your code
-        pass
-    
+        cells = [ [0 for col in range(self.get_grid_width())] for row in range(self.get_grid_height())]
+        self._grid = cells
+        return cells
+        
     def __str__(self):
         """
         Return a string representation of the grid for debugging.
         """
-        # replace with your code
-        pass
+        string = ''
+        for row in self._grid:
+            string += str(row) + '\n'
+        return string
 
     def get_grid_height(self):
         """
         Get the height of the board.
         """
-        # replace with your code
-        return 0
+        return self._grid_height
     
     def get_grid_width(self):
         """
         Get the width of the board.
         """
-        # replace with your code
-        return 0
+        return self._grid_width
                             
     def move(self, direction):
         """
@@ -87,6 +87,22 @@ class TwentyFortyEight:
         """
         # replace with your code
         pass
+
+    def get_empty_tiles(self):
+        """
+        Get a list of tuples representing all of the 
+        empty tiles that contain zeros.
+        """
+        empty_squares = []
+        row_num = 0
+        for row in self._grid:
+            column_num = 0 
+            for column in row:
+                if column == 0:
+                    empty_squares.append((row_num, column_num))
+                column_num += 1 
+            row_num += 1
+        return empty_squares
         
     def new_tile(self):
         """
@@ -94,22 +110,22 @@ class TwentyFortyEight:
         square.  The tile should be 2 90% of the time and
         4 10% of the time.
         """
-        # replace with your code
-        pass
-        
+        empty_tiles = self.get_empty_tiles() 
+        random_empty_tile = random.choice(empty_tiles)
+        value = random.choice([2] * 9 + [4])
+        self.set_tile(random_empty_tile[0], random_empty_tile[1], value)
+                
     def set_tile(self, row, col, value):
         """
         Set the tile at position row, col to have the given value.
-        """        
-        # replace with your code
-        pass
+        """ 
+        self._grid[row][col] = value     
 
     def get_tile(self, row, col):
         """
         Return the value of the tile at position row, col.
         """        
-        # replace with your code
-        return 0
+        return self._grid[row][col]
  
     
 #poc_2048_gui.run_gui(TwentyFortyEight(4, 4))
--- Principles_of_Computing/poc_2048_testsuite.py
+++ Principles_of_Computing/poc_2048_testsuite.py
@@ -32,11 +32,48 @@ def run_test(game_class):
     # Create a TestSuite object
     suite = poc_simpletest.TestSuite()
 
-    # Create a game
-    game = game_class(6, 6)
-    
-    # Test is_game_won
-    suite.run_test(game.test(), "foobar", "test")                     
+    # Test 1
+    game1 = game_class(4, 4)
+    game1.reset()
+    game1.set_tile(1, 1, 4)
+    game1.new_tile()
+    print(game1)
+    suite.run_test(game1.get_tile(1, 1), 4, "Test game1 get_tile()")
+    suite.run_test(game1.get_tile(2, 3), 0, "Test game1 get_tile()")
+    suite.run_test(game1.reset(), [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]], "Test game1 reset()")   
+
+    # Test 2
+    game2 = game_class(3, 3)
+    game2.reset()
+    game2.set_tile(2, 2, 7)
+    print(game2)
+    suite.run_test(game2.get_tile(2, 2), 7, "Test game2 get_tile()")
+    suite.run_test(game2.get_tile(1, 1), 0, "Test game2 get_tile()") 
+    suite.run_test(game2.reset(), [[0, 0, 0], [0, 0, 0], [0, 0, 0]], "Test game2 reset()") 
+
+    # Test 3
+    game3 = game_class(2, 7)
+    game3.reset()
+    game3.set_tile(0, 4, 3)
+    print(game3)
+    suite.run_test(game3.get_tile(0, 4), 3, "Test game3 get_tile()")
+    suite.run_test(game3.get_tile(1, 6), 0, "Test game3 get_tile()") 
+    suite.run_test(game3.reset(), [[0, 0, 0, 0, 0, 0, 0 ], [0, 0, 0, 0, 0, 0, 0]], "Test game3 reset()")  
+
+    # Test 4: Test new_tile method 
+    game4 = game_class(3, 3)
+    game4.reset()
+    game4.set_tile(0, 0, 8)
+    game4.set_tile(0, 1, 8)
+    game4.set_tile(0, 2, 8)
+    game4.set_tile(1, 0, 8)
+    game4.set_tile(1, 1, 8)
+    game4.set_tile(1, 2, 8)
+    game4.new_tile()
+    print(game4)
+    #suite.run_test(game4.get_tile(2, 2), 7, "Test game4 get_tile()")
+    #suite.run_test(game4.get_tile(1, 1), 0, "Test game4 get_tile()") 
+    #suite.run_test(game4.reset(), [[0, 0, 0], [0, 0, 0], [0, 0, 0]], "Test game4 reset()")  
 
     # Report number of tests and failures
     suite.report_results()
