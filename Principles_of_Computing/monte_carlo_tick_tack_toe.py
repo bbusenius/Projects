e5584862e80ca247e9b32fbb04f1eb2c54072ca9
Brad Busenius <bbusenius@users.noreply.github.com>
2014-06-26 18:10:36 -0500

    Completed the mc_trial function and started work on a score_board function in monte_carlo_tick_tack_toe.py.

--- Principles_of_Computing/monte_carlo_tick_tack_toe.py
+++ Principles_of_Computing/monte_carlo_tick_tack_toe.py
@@ -11,8 +11,9 @@ import poc_ttt_provided as provided
 NTRIALS = 1    # Number of trials to run
 MCMATCH = 1.0  # Score for squares played by the machine player
 MCOTHER = 1.0  # Score for squares played by the other player
+
+SCORES = []
     
-# Add your functions here.
 def mc_trial(board, player):
     """
     Play a game of random moves, alternating between players.
@@ -26,8 +27,69 @@ def mc_trial(board, player):
     Returns:
         None (but alters the game state) 
     """
-    print board.get_empty_squares()
-    print player
+    original_player = player
+
+    # Get all empty sqares on the board 
+    empty_squares = board.get_empty_squares()
+
+    # Randomly select an empty square
+    for move in range(len(empty_squares)): 
+        square = random.choice(empty_squares)
+        
+        # Player makes a move 
+        board.move(square[0], square[1], player)
+
+        # Remove a square after using it
+        empty_squares.remove(square)
+
+        # Switch players 
+        player = provided.switch_player(player)
+
+        # Stop the game if there's a winner
+        if board.check_win(): 
+            break
+
+    score_board(board, original_player) 
+    return None
+
+def score_board(board, player):
+    """
+    Scores each square on a given board. 
+
+    Args:
+        board: object, a bord to score.
+
+        player: the player to score for
+
+    Returns: 
+        scores: a list of scores for each square in the board.
+    """
+    winner = board.check_win() 
+    print "WINNER:", winner
+    print board
+    scores = []
+    for row in range(board.get_dim()):
+        for col in range(board.get_dim()):
+            # Game is a tie or square is empty, score 0
+            if winner == 4 or board.square(row, col) == 1:
+                scores.append((row, col, 0))
+            # The player won the game
+            elif winner == player:
+                # The square belongs to the player 
+                if board.square(row, col) == player:
+                    scores.append((row, col, MCMATCH))
+                # The square belongs to the other player
+                elif board.square(row, col) == provided.switch_player(player):
+                    scores.append((row, col, -MCOTHER))
+            # The player lost the game
+            elif winner == provided.switch_player(player):
+                # The square belongs to the player 
+                if board.square(row, col) == player:
+                    scores.append((row, col, -MCMATCH))
+                 # The square belongs to the other player
+                elif board.square(row, col) == provided.switch_player(player):
+                    scores.append((row, col, MCOTHER))
+    print scores
 
 def mc_update_scores(scores, board, player):
     """
