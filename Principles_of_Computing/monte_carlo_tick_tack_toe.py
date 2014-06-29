435824ccc131b934de22de0f811e642e97d0f184
Brad Busenius <bbusenius@users.noreply.github.com>
2014-06-29 01:09:58 -0500

    Finished Montey Carlo Tick-Tack-Toe.

--- Principles_of_Computing/monte_carlo_tick_tack_toe.py
+++ Principles_of_Computing/monte_carlo_tick_tack_toe.py
@@ -8,19 +8,25 @@ import poc_ttt_provided as provided
 
 # Constants for Monte Carlo simulator
 # Change as desired
-NTRIALS = 500    # Number of trials to run
+NTRIALS = 300    # Number of trials to run
 MCMATCH = 1.0  # Score for squares played by the machine player
 MCOTHER = 1.0  # Score for squares played by the other player
 
-# Lookup by winner, tie, or empty in the dictionsry 
-# 1 is empty, 2 is oppoent, 3 is player, 4 is a tie
-# Within each dictionary lookup by the player who 
-# "owns" the square. 1 is empty, 2 is the opponent, 
-# 3 is the current player.
-SQUARE_SCORE = {1 : {3 : 0.0, 2 : 0.0, 1 : 0.0 },
-                2 : {3 : -MCMATCH, 2 : MCOTHER, 1 : 0.0},
-                3 : {3 : MCMATCH, 2 : -MCOTHER, 1 : 0.0},
-                4 : {3 : 0.0, 2 : 0.0, 1 : 0.0}}
+# Lookup by winner tie, or empty in the outer dictionary. 
+# Within the inner dictionary lookup by the player who 
+# "owns" the square.
+SQUARE_SCORE = {provided.EMPTY   : {provided.PLAYERO : 0, 
+                                    provided.PLAYERX : 0, 
+                                    provided.EMPTY   : 0 },
+                provided.PLAYERX : {provided.PLAYERO : -MCOTHER, 
+                                    provided.PLAYERX : MCMATCH, 
+                                    provided.EMPTY   : 0},
+                provided.PLAYERO : {provided.PLAYERO : MCOTHER, 
+                                    provided.PLAYERX : -MCMATCH, 
+                                    provided.EMPTY   : 0},
+                provided.DRAW    : {provided.PLAYERO : 0, 
+                                    provided.PLAYERX : 0, 
+                                    provided.EMPTY   : 0}}
     
 def mc_trial(board, player):
     """
@@ -74,7 +80,7 @@ def mc_update_scores(scores, board, player):
     """
     winner = board.check_win()
     # Loop through the grid and update the scores grid by doing a dictionary lookup.
-    # Lookup by winner > player to get the correct score for each square.
+    # Lookup by winner -> player to get the correct score for each square.
     for row in range(board.get_dim()):
         for col in range(board.get_dim()):
             player = board.square(row, col)
@@ -115,6 +121,7 @@ def get_best_move(board, scores):
             coordinates = (row, col)
             if coordinates in empty_squares and scores[row][col] == highest_score:
                 highest_scores.append((row, col))
+
     return random.choice(highest_scores)
 
 def mc_move(board, player, trials):
@@ -131,18 +138,14 @@ def mc_move(board, player, trials):
     Returns:
         tuple, (row, column)
     """
-    # Clone the board to use for monte carlo practice games 
-    # and setup a list of null scores. 
-    scores = [[0] * board.get_dim()] * board.get_dim()
-
-    # Leave the function if there is no next move
-    if board.check_win():
-        return None
-
+    # Setup a list of null scores. 
+    scores = [[0 for dummy_row in range(board.get_dim())] for dummy_col in range(board.get_dim())]
+    # Run through a bunch of trials and update the score grid accordingly. 
     for dummy_game in range(trials):
         clone = board.clone()
         mc_trial(clone, player)
         mc_update_scores(scores, clone, player)
+    # Return the best move according to the score grid.
     return get_best_move(board, scores)
         
 
@@ -151,12 +154,16 @@ def mc_move(board, player, trials):
 # Both should be commented out when you submit for
 # testing to save time.
 
-# provided.play_game(mc_move, NTRIALS, False)        
+#provided.play_game(mc_move, NTRIALS, False)        
 # poc_ttt_gui.run_gui(3, provided.PLAYERX, mc_move, NTRIALS, False)
+
 #import monte_carlo_testsuite as test_ttt
 #test_ttt.test_trial(mc_trial)
 #test_ttt.test_update_scores(mc_update_scores, MCMATCH, MCOTHER)
 #test_ttt.test_best_move(get_best_move)
 
-import monte_carlo_testsuite2 as wopr
-wopr.test_mc_move(mc_move)
+#import monte_carlo_testsuite2 as wopr
+#wopr.test_mc_trial(mc_trial)
+#wopr.test_mc_update_scores(mc_update_scores)
+#wopr.test_get_best_move(get_best_move)
+#wopr.test_mc_move(mc_move)
