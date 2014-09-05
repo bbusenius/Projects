e88320e788ebf3e5148010ce7adf6910ff78c49f
Brad Busenius <bbusenius@users.noreply.github.com>
2014-09-05 18:25:29 -0500

    Changed the name of the ER algorithm function to random_undirected_graph, wrote a function for generating a random directed graph using the modified ER algorithm, wrote a function for plotting the in-degree distribution for generic graphs.

--- Algorithmic_Thinking/alg_load_graph.py
+++ Algorithmic_Thinking/alg_load_graph.py
@@ -49,7 +49,8 @@ def load_graph(graph_url):
 
 def plot_citation_graph():
     """
-    Function for plotting the citation graph.
+    Function for plotting the in-degree distribution of 
+    a specific citation graph.
     """
     # Load the external graph of citation information
     citation_graph = load_graph(CITATION_URL)
@@ -67,8 +68,11 @@ def plot_citation_graph():
     plt.xlabel('Papers - base 10')  
     plt.show()
 
-
-def ER_algorithm(num_nodes, probability):
+def random_undirected_graph(num_nodes, probability):
+    """
+    Algorithm for generating random undirected graphs
+    (ER algorithm).
+    """
 
     # All possible nodes 
     all_nodes = range(num_nodes)
@@ -76,7 +80,7 @@ def ER_algorithm(num_nodes, probability):
     # Dynamically create a graph
     graph = {}
     for key in range(num_nodes):
-        graph[key] = []
+        graph[key] = set([])
 
     # Loop over every possible combination of 
     # nodes and build an edge between them 
@@ -86,9 +90,74 @@ def ER_algorithm(num_nodes, probability):
             if possible_node != current_node:
                 a = random.uniform(0, 1.0)
                 if a < probability:
-                    graph[current_node].append(possible_node)
+                    graph[current_node].add(possible_node)
     return graph
 
-print ER_algorithm(100, 0.4)
+def random_directed_graph(num_nodes, probability):
+    """
+    Algorithm for generating random directed graphs 
+    (Modified ER algorithm).
+    """
+
+    def random_num():
+        """
+        Return a random number.
+        """ 
+        return random.uniform(0, 1.0)
+
+    # All possible nodes 
+    all_nodes = range(num_nodes)
+
+    # Dynamically create a graph without edges
+    graph = {}
+    for key in range(num_nodes):
+        graph[key] = set([])
+
+    # Loop over every possible combination of 
+    # nodes and build an edge between them 
+    # based on the probability
+    for current_node in range(num_nodes):
+        for possible_node in all_nodes:
+            if possible_node != current_node:
+                a = random_num()
+                if a < probability:
+                    graph[current_node].add(possible_node)
+                b = random_num() 
+                if b < probability:
+                     graph[possible_node].add(current_node)
+    return graph
+
+def plot_graph(graph, title='', xlabel='', ylabel=''):
+    """
+    Function uses matplotlib to create a log/log plot of the 
+    in-degree distribution of a graph.
+
+    Args: 
+        graph: a dictionary representation of a directed graph
+        where the keys are node names and the values are sets
+        of edges.
+
+    Returns:
+        None and plots a log/log plot of the graph.
+    """ 
+    # Calculate the in-degree distribution of the citation graph
+    in_degree_distribution = dd.in_degree_distribution(graph)
+
+    # Normalize the in-degree distribution to sum up to 1
+    normalized_distribution = dd.normalize_distribution(in_degree_distribution)
+
+     
+    # Plot the normalized distribution on a graph
+    #plt.plot(normalized_distribution.keys(), normalized_distribution.values(), 'ro') 
+    plt.loglog(normalized_distribution.keys(), normalized_distribution.values(), 'bo', basex=10, basey=10)
+
+    # Add attributes to the plot
+    plt.title(title) 
+    plt.xlabel(xlabel)  
+    plt.ylabel(ylabel)  
+    plt.show()
+
+
+plot_graph(random_directed_graph(2770, 0.5), 'Log/log plot of the in-degree distribution of the ER graph', 'Nodes (base 10)', 'In-degrees (base 10)')
 
 #plot_citation_graph()
